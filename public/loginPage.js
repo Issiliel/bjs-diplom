fa2aeb5b69a3606a346b39f947eb3b22c38034e5
Elena Voronina <voronina013@gmail.com>
2022-10-23 14:44:49 +0300

    correct loginPage

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,19 +2,21 @@
 const userForm = new userForm();
 
 userForm.loginFormCallback = function (data) {
-    ApiConnector.login(data, response => {
-        if (!response.success) {
+    ApiConnector.login(data, (response) => {
+        if (response.success) {
+            location.reload();
+        } else {
             userForm.setLoginErrorMessage(response.error);
         }
-        location.reload();
     });
 };
 
 userForm.registerFormCallback = function (data) {
-    ApiConnector.register (data, response => {
-        if(!response.succes) {
+    ApiConnector.register(data, (response) => {
+        if (response.success) {
+            location.reload();
+        } else {
             userForm.setRegisterErrorMessage(response.error);
-        }  
-        location.reload();
+        }
     });
 };
